43d0f1012a66700d04abded0a6a08d23b542dca6
Ian Cooper <Neuralsilicon@gmail.com>
2021-06-26 01:19:45 -0500

    password encryption test

--- FuelRateTests/PasswordTest.swift
+++ FuelRateTests/PasswordTest.swift
@@ -4,6 +4,24 @@ import XCTest
 @testable import FuelRate
 
 class PasswordTests: XCTestCase {
+    var encrytion:Encryption!
+    
+    
+    override func setUpWithError() throws {
+      try super.setUpWithError()
+        encrytion = Encryption()
+    }
 
+    override func tearDownWithError() throws {
+        encrytion = nil
+      try super.tearDownWithError()
+    }
+    
+    func testPrice() {
+        
+        XCTAssertNoThrow(try encrytion.aesDecrypt(KEY: "keykeykeykeykeyk", IV: "drowssapdrowssap", password: "Hello"))
+        XCTAssertNoThrow(try encrytion.aesEncrypt(KEY: "keykeykeykeykeyk", IV: "drowssapdrowssap", password: "Hello"))
+    }
+    
     
 }
