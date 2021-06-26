faf210aad3840f38b4972771f8ddaa4950870eba
Ian Cooper <Neuralsilicon@gmail.com>
2021-06-26 00:48:50 -0500

    new model encryption

--- FuelRate/Model/Client/PasswordEncryption.swift
+++ FuelRate/Model/Client/PasswordEncryption.swift
@@ -1,1 +1,22 @@
 
+//Ian cooper
+
+import UIKit
+import CryptoSwift
+
+
+class Encryption{
+    
+    func aesEncrypt(KEY:String, IV:String, password:String) throws -> String {
+        let encrypted = try AES(key: KEY, iv: IV, padding: .pkcs7).encrypt([UInt8](password.data(using: .utf8)!))
+        return Data(encrypted).base64EncodedString()
+    }
+
+    func aesDecrypt(KEY:String, IV:String, password:String) throws -> String {
+        guard let data = Data(base64Encoded: password) else { return "" }
+        let decrypted = try AES(key: KEY, iv: IV, padding: .pkcs7).decrypt([UInt8](data))
+        return String(bytes: decrypted, encoding: .utf8) ?? password
+    }
+}
+
+
--- Podfile
+++ Podfile
@@ -4,6 +4,7 @@ target 'FuelRate' do
   pod 'Firebase/Core', :inhibit_warnings => true
   pod 'Firebase/Database', :inhibit_warnings => true
   pod 'Firebase/Auth', :inhibit_warnings => true
+  pod 'CryptoSwift', '~> 1.4.0'
 
 end
 
