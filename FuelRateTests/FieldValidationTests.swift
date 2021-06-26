a6eda6d703e07cb66807d92b5d1a6b5f009c1acb
Ian Cooper <Neuralsilicon@gmail.com>
2021-06-25 23:59:27 -0500

    Unit tests are done

--- FuelRateTests/FieldValidationTests.swift
+++ FuelRateTests/FieldValidationTests.swift
@@ -1,1 +1,64 @@
 
+//Ian Cooper
+
+import XCTest
+@testable import FuelRate
+
+class FieldValidationTests: XCTestCase {
+    var validation:Validation!
+    
+    
+    override func setUpWithError() throws {
+      try super.setUpWithError()
+        validation = Validation()
+    }
+
+    override func tearDownWithError() throws {
+        validation = nil
+      try super.tearDownWithError()
+    }
+    
+    func testClient() {
+ 
+        var client = Client()
+        client.fullname = "Jack"
+        client.password = "Abscss44$"
+        client.username = "a@gmail.com"
+        
+        XCTAssertEqual(validation.validateformtextFields(fields: .fullname, client: client, address: nil),true, "Full name validation")
+        
+        XCTAssertEqual(validation.validateformtextFields(fields: .password, client: client, address: nil),true, "Password validation")
+        
+        XCTAssertEqual(validation.validateformtextFields(fields: .username, client: client, address: nil),true, "Username validation")
+        
+        XCTAssertNoThrow(validation.checkvalidation(client: client, address: nil, parent: UIViewController(), completed: {_ in
+        }))
+    }
+    
+    
+    func testAddress() {
+ 
+        var address = Address()
+        address.addressLineOne = "25000 JK"
+        address.addressLineTwo = "apt. 1066"
+        address.city = "Houston"
+        address.state = "TX"
+        address.zipcode = "77000"
+        
+        XCTAssertEqual(validation.validateformtextFields(fields: .addressLineOne, client: nil, address: address),true, "Address line One validation")
+        
+        XCTAssertEqual(validation.validateformtextFields(fields: .addressLineTwo, client: nil, address: address),true, "Address line Two validation")
+        
+        XCTAssertEqual(validation.validateformtextFields(fields: .city, client: nil, address: address),true, "City One validation")
+        
+        XCTAssertEqual(validation.validateformtextFields(fields: .state, client: nil, address: address),true, "State One validation")
+        
+        XCTAssertEqual(validation.validateformtextFields(fields: .zipcode, client: nil, address: address),true, "Zipcode validation")
+        
+        XCTAssertNoThrow(validation.checkvalidation(client: nil, address: address, parent: UIViewController(), completed: {_ in
+        }))
+    }
+    
+    
+    
+}
--- FuelRateTests/FuelPriceTests.swift
+++ FuelRateTests/FuelPriceTests.swift
@@ -1,1 +1,77 @@
 
+//Ian Cooper
+
+import XCTest
+@testable import FuelRate
+
+class FuelPriceTests: XCTestCase {
+    var fuelPrice:FuelPriceDummy!
+    
+    
+    override func setUpWithError() throws {
+      try super.setUpWithError()
+        fuelPrice = FuelPriceDummy(fuelQuote: FuelQuote())
+    }
+
+    override func tearDownWithError() throws {
+        fuelPrice = nil
+      try super.tearDownWithError()
+    }
+    
+    func testPrice() {
+
+        var fuelQuote = FuelQuote()
+        fuelQuote.gallonsRequested = 1500
+        
+        var fuelFactor = FuelFactors()
+        fuelFactor.history = 0.01
+        fuelFactor.location = 0.02
+
+
+        fuelPrice.fuelQuote = fuelQuote
+        fuelPrice.fuelFactor = fuelFactor
+        fuelPrice.calculatePrice()
+        
+
+        XCTAssertEqual(fuelPrice.fuelQuote.totalAmountDue,2542.50, "Wrong Total Amount")
+        XCTAssertEqual(fuelPrice.fuelQuote.suggestedPricePerGallon,1.695, "Wrong suggestedPricePerGallon")
+    }
+    
+    func testPriceTwo() {
+
+        var fuelQuote = FuelQuote()
+        fuelQuote.gallonsRequested = 25
+        
+        var fuelFactor = FuelFactors()
+        fuelFactor.history = 0
+        fuelFactor.location = 0.03
+
+        fuelPrice.fuelQuote = fuelQuote
+        fuelPrice.fuelFactor = fuelFactor
+        fuelPrice.calculatePrice()
+        
+        XCTAssertEqual(fuelPrice.fuelQuote.totalAmountDue,43.5, "Wrong Total Amount")
+        XCTAssertEqual(fuelPrice.fuelQuote.suggestedPricePerGallon,1.74, "Wrong suggestedPricePerGallon")
+    }
+    
+    func testCalculate(){
+        var fuelQuote = FuelQuote()
+        fuelQuote.gallonsRequested = 25
+        
+        var fuelFactor = FuelFactors()
+        fuelFactor.history = 0
+        fuelFactor.location = 0.03
+
+        fuelPrice.fuelQuote = fuelQuote
+        fuelPrice.fuelFactor = fuelFactor
+        fuelPrice.uuid = UUID.init().uuidString
+        
+        XCTAssertNoThrow(fuelPrice.calculate())
+        
+        fuelPrice.uuid = nil
+        XCTAssertNoThrow(fuelPrice.calculate())
+        
+    }
+    
+}
+
