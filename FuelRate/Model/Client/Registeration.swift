c15c6fcd60f2c7069196e767242d5bd700a9c4fe
Ian Cooper <Neuralsilicon@gmail.com>
2021-06-25 23:55:29 -0500

    Model layer Client-Side is done.

--- FuelRate/Model/Client/Registeration.swift
+++ FuelRate/Model/Client/Registeration.swift
@@ -1,1 +1,47 @@
 
+// Ian cooper
+
+
+import Foundation
+import Firebase
+import os
+
+protocol RegisterationDelegate {
+    func savedClient(value:Bool)
+    func createdUser(value:Bool)
+}
+
+class Registeration{
+    
+    var client:Client!
+    var address:Address!
+    var ref: DatabaseReference!
+    var delegate:RegisterationDelegate?
+    
+    init(client:Client, address:Address) {
+        ref = Database.database(url: "https://fuelrate-7520e-default-rtdb.firebaseio.com/").reference(); self.client = client
+        self.address = address
+    }
+    
+    func createUser(){
+         Auth.auth().createUser(withEmail: (self.client.username), password: (self.client.password)) { (result, error) in
+            if let _eror = error {
+                print(_eror.localizedDescription )
+                self.delegate?.createdUser(value: false)
+            }else{
+                UserDefaults.standard.setValue(Auth.auth().currentUser!.uid, forKey: "uuid")
+                UserDefaults.standard.setValue(self.client.password, forKey: "password")
+                self.delegate?.createdUser(value: true)
+            }
+        }
+    }
+    
+    func saveClient(){
+        self.ref.child("Client").child(self.client.uuid).child("User").setValue(self.client.asPropertyList())
+        self.ref.child("Client").child(self.client.uuid).child("Address").setValue(self.address.asPropertyList())
+  
+        self.delegate?.savedClient(value: true)
+    }
+    
+}
+
